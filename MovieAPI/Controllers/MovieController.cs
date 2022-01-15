b74803e5610fc2dc1bf609e7242005f7b6727965
Yarego Brozek <yarego.brozek@lucaspaganini.com>
2022-01-15 20:49:05 -0300

    creating the movie controller & movie model

---
+++ MovieAPI/Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieAPI.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MovieAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private static List<Movie> movies = new List<Movie>();
+
+        [HttpPost]
+        public void AddMovie([FromBody]Movie movie)
+        {
+            movies.Add(movie);
+            Console.WriteLine(movie.Title);
+        }
+    }
+}
---
+++ MovieAPI/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieAPI.Models
+{
+    public class Movie
+    {
+        [Required(ErrorMessage = "The field Title is required")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "The field Director is required")]
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        [Range(1, 600, ErrorMessage = "The field Duration should have the value between 1 and 600 is required")]
+        public int Duration { get; set; }
+    }
+}
